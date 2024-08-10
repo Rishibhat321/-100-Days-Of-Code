e3f1a06a2cb64072f1e1fd7a7395fb65c04591e2
Rishib#321 <rishitab2209@gmail.com>
2024-08-10 22:05:24 +0530

    Add new

--- Day-38/src/LargestPositiveIntegerThatExistsWithItsNegative_usingHashMap_Brute.java
+++ Day-38/src/LargestPositiveIntegerThatExistsWithItsNegative_usingHashMap_Brute.java
@@ -5,7 +5,7 @@ public class LargestPositiveIntegerThatExistsWithItsNegative_usingHashMap_Brute 
 
     public static int findMaxK(int[] nums) {
 
-        HashSet<Integer> set = new HashSet<Integer>();
+   /*     HashSet<Integer> set = new HashSet<Integer>();
 
         for(int i=0;i<nums.length;i++)
         {
@@ -29,6 +29,28 @@ public class LargestPositiveIntegerThatExistsWithItsNegative_usingHashMap_Brute 
         }
 
         return k;
+
+          */
+
+        int res = -1;
+        int n = 0;
+
+        HashSet<Integer> set = new HashSet<Integer>();
+
+        for(int i=0;i<nums.length;i++)
+        {
+            set.add(nums[i]);
+        }
+
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]>0 && set.contains(-1*nums[i])){
+
+                res = Math.max(res, nums[i]);
+            }
+        }
+
+        return res;
 
     }
     public static void main(String[] args) {
